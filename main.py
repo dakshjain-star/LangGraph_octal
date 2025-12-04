3ff63c0ff0f0a8255bcb821d2ff14239652b053a
Daksh Jain <daksh.jain@octalsoftware.com>
2025-12-04 11:11:05 +0530

    fix import error

--- main.py
+++ main.py
@@ -16,4 +16,4 @@ from chatbot import api_app
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(api_app, host="0.0.0.0", port=8081, reload=True)
+    uvicorn.run("main:api_app", host="0.0.0.0", port=8081, reload=True)
